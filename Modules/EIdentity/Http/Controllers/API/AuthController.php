6aca1a0c0c0fb0cd24aac2754b723b6abd329fa3
unknown <imranemi143@gmail.com>
2023-03-22 11:02:41 +0500

    otp

--- Modules/EIdentity/Http/Controllers/API/AuthController.php
+++ Modules/EIdentity/Http/Controllers/API/AuthController.php
@@ -6,9 +6,15 @@ namespace Modules\EIdentity\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Modules\EIdentity\Http\Requests\Auth\LoginRequest;
+use Modules\EIdentity\Http\Requests\Auth\ResendOtpRequest;
+use Modules\EIdentity\Http\Requests\Auth\OtpVerificationRequest;
+use Modules\EIdentity\Http\Requests\Auth\PasswordChangeRequest;
 use Modules\EIdentity\Entities\Employees;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Hash;
+
 
 class AuthController extends Controller
 {
@@ -69,6 +75,72 @@ class AuthController extends Controller
             'status' => 'success',
             'message' => 'Successfully logged out',
         ]);
+    }
+
+    // Resend Verficiation Code 
+    public function sendResetCode(ResendOtpRequest $request): JsonResponse
+    {
+        $employee = Employees::where('cnic', $request->cnic)->first();
+        if (!$employee) {
+            return sendError("Employee record not found!", [], 403);
+        }
+        //generate OTP
+        $otp = rand(1000, 9999);
+
+        //setting Expiray Time 
+        $otp_expiry = Carbon::now()->addMinutes(5);
+
+        $employee->otp = $otp;
+        $employee->otp_expiration = $otp_expiry;
+        $employee->save();
+
+        return sendResponse('', 'Code Sent Successfully');
+    }
+
+
+    // Verify Otp 
+    public function otpVerification(OtpVerificationRequest $request): JsonResponse
+    {
+
+        $user = Employees::where('cnic', $request->cnic)->first();
+        if ($user->otp == $request->otp && $user->otp_expiration > Carbon::now()) {
+            $user->otp = null;
+            $user->otp_expiration = null;
+            $user->save();
+            return sendResponse('', 'Code is valid');
+        }
+
+        return sendError("Code is expired or invalid!", [], 403);
+    }
+
+
+    /**
+     * Change user's password.
+     *
+     * @param PasswordChangeRequest $request
+     * @param PasswordAction $passwordAction
+     * @return JsonResponse
+     */
+    public function changePassword(PasswordChangeRequest $request)
+    {
+        $status = $this->changePasswordEmployee($request->cnic, $request['old_password'], $request['new_password']);
+        if ($status) {
+            return sendResponse('', 'Password Change Successfully');
+        }
+        return sendError("Failed to change Password!", [], 403);
+    }
+
+
+    public function changePasswordEmployee($cnic, $oldPassword, $newPassword): bool
+    {
+        $user = Employees::where('cnic', $cnic)->first();
+        if (Hash::check($oldPassword, $user->password)) {
+            $user->password = Hash::make($newPassword);
+            $user->save();
+            return true;
+        }
+
+        return false;
     }
 
     public function refresh()
